6601d691e0ea5e199af4103535d1b97b20cf5f82
Bullweihaidong <1819353154@qq.com>
2018-12-14 16:02:39 +0800

    更新时间:2018年12月14日-16:02:39

--- git-push.bat
+++ git-push.bat
@@ -2,5 +2,5 @@
 @title bat 交互执行git命令
 git pull origin master
 git add .
-git commit -m 更新时间:%date:~0,4%年%date:~5,2%月%date:~8,2%日
+git commit -m 更新时间:%date:~0,4%年%date:~5,2%月%date:~8,2%日-%time:~0,2%:%time:~3,2%:%time:~6,2%
 git push -u origin master
